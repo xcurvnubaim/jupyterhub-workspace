36ada07c77bb70c5fb7ba76ba3f726d5487ac2f2
xcurvnubaim <zhariffmarzuqi@gmail.com>
2026-02-23 13:53:24 +0700

    feat: make jupyter config more clean and add iddle culler in user container

---
+++ images/alphatauri/jupyter_server_config.py
@@ -0,0 +1,13 @@
+c = get_config()
+
+# Shutdown kernel after 30 minutes idle
+c.MappingKernelManager.cull_idle_timeout = 1800
+
+# Check every 60 seconds
+c.MappingKernelManager.cull_interval = 60
+
+# 🔥 This is the key line
+c.MappingKernelManager.cull_connected = True
+
+# Optional: shutdown server when no kernels & inactive
+c.ServerApp.shutdown_no_activity_timeout = 3600
